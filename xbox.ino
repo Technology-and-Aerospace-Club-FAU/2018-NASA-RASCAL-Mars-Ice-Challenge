7d80617abf4fd3363b99f526f41c5e3bc43660ae
Leo Herrera <carlosherrer2015@fau.edu>
2018-02-14 22:59:51 -0500

    Started writing base code

---
+++ xbox.ino
@@ -0,0 +1,17 @@
+#include <Stepper.h>
+#include <AccelStepper.h>
+#include <XBOXRECV.h>
+
+// pin variables
+int relay_pin = 2; //start up pin to start motors
+int led_pin = 10; //LED pin so we can indicate whats good and whats not
+// xbox controller pins
+
+
+void setup(){
+
+}
+
+void loop(){
+
+}
